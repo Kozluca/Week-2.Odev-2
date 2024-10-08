744e023cd0ceb3361c23f78126698a0d0834a132
Kozluca <okozluca.1@gmail.com>
2024-10-08 18:47:57 +0300

    Method uygulama

---
+++ Pratik-Ilk-metot-Uygulaması/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pratik_Ilk_metot_Uygulaması
+{
+    internal class Program
+    {
+        static public void SARKI()
+        {
+            Console.WriteLine("    YILDIZLAR YERİNDE GÜZEL BIRAK DURSUN     ");
+        }
+        static int Kalan()
+        {
+            Random rnd = new Random();
+            int rastgele = rnd.Next(100);
+            Console.WriteLine("RASTGELE SAYI: " + (rastgele));
+            Console.WriteLine("Kalan sayı :" + (rastgele / 2));
+            return rastgele / 2;
+        }
+        static int Carpma(int a, int b)
+        {
+            Console.WriteLine("ÇARPMA: " + (a * b));
+            return a * b;
+        }
+        static void Isimsoyisimyaz(string isim, string soyisim)
+        {
+
+            Console.WriteLine("İsim: " + isim + " Soy isim: " + soyisim);
+        }
+        static void Main(string[] args)
+        {
+            Kalan();
+            Carpma(12, 5);
+            SARKI();
+            Isimsoyisimyaz("ÖMER", "KOZLUCA");
+            Console.ReadKey();
+        }
+    }
+
+}
